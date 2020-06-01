3f232007a06ac4d753d8eb750ff3aa8f30da83c8
ugyanakkor <ifjutibor@gmail.com>
2020-06-01 12:22:37 +0200

    feat(todo-list): Add list elements with JS

---
+++ app.js
@@ -0,0 +1,35 @@
+// Selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// Event Listeners
+todoButton.addEventListener('click', addTodo);
+
+// Functions
+function addTodo(event) {
+    // Prevent form from submitting
+    event.preventDefault();
+    // Todo div
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+    // Create LI
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    // Check mark button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add("complete-btn");
+    todoDiv.appendChild(completedButton);
+    // Thrash mark button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add("complete-btn");
+    todoDiv.appendChild(trashButton);
+    // Append todo list
+    todoList.appendChild(todoDiv);
+    todoInput.value = "";
+}
+
