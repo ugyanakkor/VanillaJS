669e6424844c6dcdd877c485f46e7f616abd7ce4
ugyanakkor <ifjutibor@gmail.com>
2020-06-01 15:15:48 +0200

    feat(todo-list): Add checkmark toogle & delete animation

--- app.js
+++ app.js
@@ -40,7 +40,17 @@ function deleteCheck(e) {
     // delete todo
     if(item.classList[0] === 'trash-btn') {
         const todo = item.parentElement;
-        todo.remove();
+        // Animation
+        todo.classList.add("fall");
+        todo.addEventListener('transitionend', function() {
+            todo.remove();
+        });
+    }
+
+    //check mark
+    if(item.classList[0] === 'complete-btn') {
+        const todo = item.parentElement;
+        todo.classList.toggle('completed');
     }
 }
 
